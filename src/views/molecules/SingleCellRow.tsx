1df9913b4f151075e11d878f9b9cc71ac334e266
DBC-Works <contact.d.b.c@gmail.com>
2020-08-18 23:54:50 +0900

    update: `SingleCellRow` Component
    * update JSDoc description
    #16

--- src/views/molecules/SingleCellRow.tsx
+++ src/views/molecules/SingleCellRow.tsx
@@ -8,6 +8,9 @@ import '@rmwc/grid/styles'
 
 /**
  * 'Single cell row' component
+ * This component cannot be the first child element of a Grid component.
+ * Because when the first child element of the Grid component is not a GridRow component,
+ * the Grid component wraps its children in the GridRow component, which breaks the layout.
  */
 const SingleCellRow: React.FC<{
   cellClassName?: string
